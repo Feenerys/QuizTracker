552e0ba859e900b9050b8caf7c2e3d47927e9d52
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-05-11 18:22:31 +1000

    feat: implement data class StudentsWithAverageMark

--- app/src/main/java/com/fit2081/quiztracker/data/quizAttempts/StudentsWithAverageMark.kt
+++ app/src/main/java/com/fit2081/quiztracker/data/quizAttempts/StudentsWithAverageMark.kt
@@ -1,4 +1,7 @@
 package com.fit2081.quiztracker.data.quizAttempts
 
 class StudentsWithAverageMark {
+    var studentId: String = ""
+    var studentName: String = ""
+    var averageMark: Double = 0.0
 }
