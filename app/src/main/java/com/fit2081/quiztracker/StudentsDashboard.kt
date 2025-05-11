51f5516e50cb3b5040963847c92f6ca9d2dc235b
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-05-11 19:34:58 +1000

    refractor: make StudentsDashboard into an activity rather than a simple kotlin class

--- app/src/main/java/com/fit2081/quiztracker/StudentsDashboard.kt
+++ app/src/main/java/com/fit2081/quiztracker/StudentsDashboard.kt
@@ -1,14 +1,20 @@
 package com.fit2081.quiztracker
 
+import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,109 +25,121 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fit2081.quiztracker.data.AuthManager
 import com.fit2081.quiztracker.data.quizAttempts.QuizAttempt
+import com.fit2081.quiztracker.ui.theme.QuizTrackerTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import kotlin.random.Random
 
-@Composable
-fun StudentsDashboard() {
-    Column() {
-        // Mutable state variables to track checkbox states for each question
-        var q1 by remember { mutableStateOf(false) }
-        var q2 by remember { mutableStateOf(false) }
-        var q3 by remember { mutableStateOf(false) }
+class StudentsDashboard : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuizTrackerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        // Mutable state variables to track checkbox states for each question
+                        var q1 by remember { mutableStateOf(false) }
+                        var q2 by remember { mutableStateOf(false) }
+                        var q3 by remember { mutableStateOf(false) }
 
-        // Get the current context
-        val _context = LocalContext.current
+                        // Get the current context
+                        val _context = LocalContext.current
 
-        // Display the quiz title with styling
-        Text (
-            text = "FIT2081 Mobile Quiz",
-            style = TextStyle(
-                fontSize = 30.sp,
-                fontWeight = androidx.compose.ui.text.font.FontWeight.Bold
-            )
-        )
+                        // Display the quiz title with styling
+                        Text (
+                            text = "FIT2081 Mobile Quiz",
+                            style = TextStyle(
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
 
-        Spacer(modifier = Modifier.padding(10.dp))
+                        Spacer(modifier = Modifier.padding(10.dp))
 
-        Text("Select the correct answers to the following statements")
-        Spacer(modifier = Modifier.padding(20.dp))
+                        Text("Select the correct answers to the following statements")
+                        Spacer(modifier = Modifier.padding(20.dp))
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Checkbox(
-                checked = q1,
-                onCheckedChange = {q1 = it}
-            )
-            Text("Paris is the capitale of France.")
-        }
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Checkbox(
+                                checked = q1,
+                                onCheckedChange = {q1 = it}
+                            )
+                            Text("Paris is the capitale of France.")
+                        }
 
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Checkbox(
-                checked = q2,
-                onCheckedChange = {q2 = it}
-            )
-            Text("Vincent van Gogh painted the Mona Lisa")
-        }
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Checkbox(
+                                checked = q2,
+                                onCheckedChange = {q2 = it}
+                            )
+                            Text("Vincent van Gogh painted the Mona Lisa")
+                        }
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Checkbox(
-                checked = q3,
-                onCheckedChange = {q3 = it}
-            )
-            Text("Mount Kosciuszko is the highest mountain in Australia")
-        }
-        Spacer(modifier = Modifier.padding(20.dp))
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Checkbox(
+                                checked = q3,
+                                onCheckedChange = {q3 = it}
+                            )
+                            Text("Mount Kosciuszko is the highest mountain in Australia")
+                        }
+                        Spacer(modifier = Modifier.padding(20.dp))
 
-        Button(
-            onClick = {
-                var totalMark = 0.0
-                if (q1) totalMark += 1
-                if (!q2) totalMark += 1
-                if (q3) totalMark += 1
+                        Button(
+                            onClick = {
+                                var totalMark = 0.0
+                                if (q1) totalMark += 1
+                                if (!q2) totalMark += 1
+                                if (q3) totalMark += 1
 
-                var random4digits = Random.nextInt(1000, 9999)
+                                var random4digits = Random.nextInt(1000, 9999)
 
-                var quizId = "Qz$random4digits"
-                val dateFormat = SimpleDateFormat("dd/MM/yyyy")
-                val currentDate = dateFormat.format(System.currentTimeMillis())
+                                var quizId = "Qz$random4digits"
+                                val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+                                val currentDate = dateFormat.format(System.currentTimeMillis())
 
-                var attempt: QuizAttempt = QuizAttempt(
-                    studentId = AuthManager.getStudentId().toString(),
-                    quizId = quizId,
-                    quizDate = currentDate,
-                    finalMark = totalMark
-                )
+                                var attempt: QuizAttempt = QuizAttempt(
+                                    studentId = AuthManager.getStudentId().toString(),
+                                    quizId = quizId,
+                                    quizDate = currentDate,
+                                    finalMark = totalMark
+                                )
 
-                CoroutineScope(Dispatchers.IO).launch {
-                    quizAttemptViewMode.insertQuizAttempt(attempt)
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    quizAttemptViewMode.insertQuizAttempt(attempt)
+                                }
+
+                                q1 = false
+                                q2 = false
+                                q3 = false
+                                Toast.makeText(_context, "Quiz Attempt Submitted", Toast.LENGTH_SHORT).show()
+                            }, modifier = Modifier.align(Alignment.End)
+                        ) {
+                            Text("Submit Quiz Attempt")
+                        }
+                        HorizontalDivider()
+
+                    }
                 }
-
-                q1 = false
-                q2 = false
-                q3 = false
-                Toast.makeText(_context, "Quiz Attempt Submitted", Toast.LENGTH_SHORT).show()
-            }, modifier = Modifier.align(Alignment.End)
-        ) {
-            Text("Submit Quiz Attempt")
+            }
         }
-        HorizontalDivider()
-        
     }
 }
