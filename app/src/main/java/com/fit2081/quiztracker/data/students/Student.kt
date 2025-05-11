07e50e64d9c0c93e972c29c5165596e864c68e4e
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-05-11 17:54:07 +1000

    feat: add student class and dependencies

--- app/src/main/java/com/fit2081/quiztracker/data/students/Student.kt
+++ app/src/main/java/com/fit2081/quiztracker/data/students/Student.kt
@@ -1,4 +1,13 @@
 package com.fit2081.quiztracker.data.students
 
-class Student {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Student entity within the Room database
+@Entity(tableName = "students")
+data class Student (
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val studentName: String,
+    val studentPassword: String,
+    val studentId: String = ""
+)
