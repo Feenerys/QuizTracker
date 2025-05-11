04d0445715a01127d96f0b2ac54901f583a3af38
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-05-11 18:33:07 +1000

    feat: implement QuizAttemptsRepository

--- app/src/main/java/com/fit2081/quiztracker/data/quizAttempts/QuizAttemptsRepository.kt
+++ app/src/main/java/com/fit2081/quiztracker/data/quizAttempts/QuizAttemptsRepository.kt
@@ -1,4 +1,23 @@
 package com.fit2081.quiztracker.data.quizAttempts
 
-class QuizAttemptsRepository {
+import android.content.Context
+import com.fit2081.quiztracker.data.CollegeDatabase
+import kotlinx.coroutines.flow.Flow
+
+class QuizAttemptsRepository(context: Context) {
+    // Create an instance of the QuizAttemptDao
+    private val quizAttemptDao =
+        CollegeDatabase.getDatabase(context).quizAttemptDao()
+
+    // Retrieve all quiz attempts from the database
+    val allAttempts: Flow<List<QuizAttempt>> = quizAttemptDao.getAllQuizAttempt()
+
+    // Insert a new quiz attempt into the database
+    suspend fun insert(attempt: QuizAttempt) {
+        quizAttemptDao.insert(attempt)
+    }
+
+    // Retrieve quiz attempts for a specific student from the database
+    fun getQuizAttemptByStudentId(studentId: String): Flow<List<QuizAttempt>> =
+        quizAttemptDao.getQuizAttemptByStudentId(studentId)
 }
