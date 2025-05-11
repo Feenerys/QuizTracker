b80d57d66f6e6d89be5057e5d5f9793bee83791f
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-05-11 19:00:41 +1000

    feat: implement AuthManager

--- app/src/main/java/com/fit2081/quiztracker/data/AuthManager.kt
+++ app/src/main/java/com/fit2081/quiztracker/data/AuthManager.kt
@@ -1,4 +1,20 @@
 package com.fit2081.quiztracker.data
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
 object AuthManager {
+    val _userId: MutableState<String?> = mutableStateOf(null)
+
+    fun login(userId: String) {
+        _userId.value = userId
+    }
+
+    fun logout() {
+        _userId.value = null
+    }
+
+    fun getStudentId(): String? {
+        return _userId.value
+    }
 }
