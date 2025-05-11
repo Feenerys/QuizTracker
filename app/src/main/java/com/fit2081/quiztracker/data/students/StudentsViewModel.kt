b0d7ba28aac9644e8eda6b90763a039a544cbf56
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-05-11 18:20:44 +1000

    feat: implement StudentsViewModel, manages student-specific logic

--- app/src/main/java/com/fit2081/quiztracker/data/students/StudentsViewModel.kt
+++ app/src/main/java/com/fit2081/quiztracker/data/students/StudentsViewModel.kt
@@ -1,4 +1,33 @@
 package com.fit2081.quiztracker.data.students
 
-class StudentsViewModel {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.fit2081.quiztracker.data.quizAttempts.StudentsWithAverageMark
+import kotlinx.coroutines.flow.Flow
+
+class StudentsViewModel(context: Context) : ViewModel() {
+    val repository = StudentsRepository(context = context)
+
+    // Retrieves a Flow of all students
+    val allStudents: Flow<List<Student>> = repository.getAllStudents()
+
+    // Inserts a new student into the database
+    suspend fun insert(student: Student) = repository.insertStudent(student)
+
+    // Retrieves a student from the database by their ID
+    suspend fun getStudentById(studentId: String): Student {
+        return repository.getStudentById(studentId)
+    }
+
+    // Retrieves a Flow of students with their average marks
+    fun getStudentsWithAverageMarks(): Flow<List<StudentsWithAverageMark>> =
+        repository.getStudentsWithAverageMarks()
+
+    // Factory class for creating instances of StudentsViewModel
+    class StudentsViewModelFactory(context: Context) : ViewModelProvider.Factory {
+        private val context = context.applicationContext
+        override fun <T : ViewModel> create(modelClass: Class<T>): T =
+            StudentsViewModel(context) as T
+    }
 }
